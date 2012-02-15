8762115ed4ee7adb4338bdc10665f909b48db04f
Jackie Jin <jacky@cloud30.com>
2012-02-15 03:05:41 -0800

    server up

--- nodejs_examples/httpChatServer/chatServer.js
+++ nodejs_examples/httpChatServer/chatServer.js
@@ -1,9 +1,39 @@
-var http = require('http');
+//require socket.io module and let the server listent to port
+var io = require('socket.io').listen(8000);
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {
-		
+//TODO: explain client-server
+//message processing and event handling on wednesday
+
+//connect socket for each logged-in user
+io.sockets.on('connection', function (socket) {
+	// listen for user registrations from client side
+	// then set the socket nickname to 
+	socket.on('register', function (name) {
+		socket.set('nickname', name, function () {
+			
+			io.sockets.emit('chat', {
+				msg : "Welcome to the Channel " + name + '!',
+				msgr : name    
+                    });
+		    });
 	    });
-				
-	
-    }).listen(4000);
+    
+    //socket.io triggers the chat event(this is a custom event) 
+	socket.on('chat', function (data) {
+		//get the username
+		var username;
+        //this cannot be called before set. Exercise:think about why.	      
+		socket.get('nickname', function (err, name) {
+			if(!username) {
+			    username = name;
+			}
+        });   
+		
+		// emit message to all users 
+        // Exercise:try use broadcast and see what happens
+		io.sockets.emit('chat', {
+			msg : data, 
+			msgr : username
+			    });
+	    });
+    });
